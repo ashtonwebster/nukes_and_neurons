5ac9f44672fbdab1fc140ef01a8fbbd19aa231f4
ashton <ashton.webster@gmail.com>
2017-04-01 16:15:55 -0400

    created scene for generating imitation learning data

---
+++ Assets/scripts/RandomGoalMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomGoalMover : MonoBehaviour {
+
+	GameObject _goal = null;
+
+	GameObject goalState {
+		get { 
+			if (_goal == null) {
+				_goal = (GameObject)Resources.Load ("prefabs/goal_cube");
+			}
+			return _goal;
+		}
+		set { _goal = value; }
+	}
+				
+
+	// Use this for initialization
+	void Start () {
+		goalState = Instantiate (goalState);
+		goalState.transform.localPosition = new Vector3 (Random.value * 10, 4, Random.value * 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// restart on click
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject.Destroy (goalState);
+			this.Start ();
+			//SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+	}
+}
--- Assets/scripts/Recorder.cs
+++ Assets/scripts/Recorder.cs
@@ -6,34 +6,82 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Recorder : MonoBehaviour {
 
-	GameObject goal;
+	GameObject child;
+	string HEADER = "XZangleToObj,YZangleToObj,DistToObj,";
 	public float logPeriod = 0f;
-	private float timeSinceLog = 0f;
+	//private float timeSinceLog = 0f;
 	private System.IO.StreamWriter file;
+
+	private GameObject _goal = null;
+
+	public GameObject goal {
+		get { 
+			if (_goal == null) {
+				_goal = GameObject.FindGameObjectWithTag ("goal_state");
+			}
+			return _goal; 
+		}
+		set { _goal = value; }
+	}
+			
 
 	// Use this for initialization
 	void Start () {
 		file = new System.IO.StreamWriter (@"/tmp/test2.txt", true);
-		goal = GameObject.Find ("goal_cube");
-		file.WriteLine(ObservedAction.GetHeader ());
+		goal = GameObject.FindGameObjectWithTag ("goal_state");
+		child = GameObject.Find ("FirstPersonCharacter");
+		// combine the features header and the target movement header
+		file.WriteLine(HEADER + ObservedAction.GetHeader ());
 
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.timeSinceLog += Time.deltaTime;
-		//if (timeSinceLog > logPeriod) {
-//			float dist = Vector3.Distance (this.transform.position, this.goal.transform.position);
-//			Debug.Log (dist);
 
-			//Debug.Log(new ObservedAction().ToString());
-			file.WriteLine(new ObservedAction().ToString());
-			file.Flush ();
+		//Debug.Log(new ObservedAction().ToString());
 
-			this.timeSinceLog = 0f;
-		//}
+		// write to file with features and target movements
+		file.WriteLine(string.Format("{0},{1},{2},{3}", 
+			GetXZangleToObj(this.goal),
+			GetYZangleToObj(this.goal),
+			GetDistToObj(this.goal),
+			new ObservedAction().ToString()));
+		file.Flush ();
+
+//		Debug.Log (string.Format("XZrot: {0}, YZrot: {1} Dist {2}", GetXZangleToObj (this.goal),
+//			GetYZangleToObj(this.goal),
+//			GetDistToObj(this.goal)));
+
 	}
+		
+	float GetXZangleToObj(GameObject obj) {
+		// project the vector of the direction we are facing to the floor
+		Vector3 forwardProjectedOnFloor = Vector3.ProjectOnPlane (this.transform.forward, Vector3.up);
+		// project the vector to the goal to the floor
+		Vector3 vectorToGoalProjected = Vector3.ProjectOnPlane (obj.transform.localPosition - this.transform.localPosition, Vector3.up);
+		// get angle between vectors (i.e. angle on XZ plane)
+		return Vector3.Angle (forwardProjectedOnFloor, vectorToGoalProjected);
+	}
+
+	/// <summary>
+	/// Gets angle to object in the YZ plane by projecting the vectors on this plane.  This is 
+	/// a bit confusing because there are two objects in the FPS controller which separately control
+	/// the up-down rotation and left right rotation!
+	/// </summary>
+	/// <returns>The Y zangle to object.</returns>
+	/// <param name="obj">Object to get angle to</param>
+	float GetYZangleToObj(GameObject obj) {
+		Vector3 forwardProjectedToObj = Vector3.ProjectOnPlane (child.transform.forward, this.transform.right);
+		Vector3 vectorToGoalProjected = Vector3.ProjectOnPlane (obj.transform.localPosition - (this.transform.localPosition + child.transform.localPosition), 
+			this.transform.right);
+		return Vector3.Angle (forwardProjectedToObj, vectorToGoalProjected);
+	}
+
+	float GetDistToObj(GameObject obj) {
+		return Vector3.Distance (this.transform.localPosition, obj.transform.localPosition);
+	}
+	
 
 	public class ObservedAction {
 		public float horizontalPan;
